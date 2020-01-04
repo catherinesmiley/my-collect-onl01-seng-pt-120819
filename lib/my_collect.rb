d3290bd1e5c0009b574f5130bb127f14a1bd9bd2
Catherine Smiley <catherinesmiley@gmail.com>
2020-01-04 20:55:36 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -29,9 +29,8 @@ def my_collect(array)
   new_array = []
   i = 0 
   while i < array.length
-    new_array << yield array[i]
+    new_array << yield(array[i])
     i += 1 
-   end
   end
   new_array
 end
